2010cf5dc84a71f6d84bd62ae82ad1c15b6d3661
Yuriy <xgooglex@bk.ru>
2024-01-18 17:25:35 +0500

    edit main

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,32 +1,56 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Department } from '../../departments/entities/department.entity';
+import { IsEmpty, IsNotEmpty } from 'class-validator';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsNotEmpty()
   @Column()
   firstName: string;
 
+  @IsNotEmpty()
   @Column()
   lastName: string;
 
+  @IsNotEmpty()
   @Column()
   surname: string;
 
+  @IsNotEmpty()
   @Column()
   username: string;
 
+  @IsNotEmpty()
   @Column()
   password: string;
 
+  @IsNotEmpty()
   @Column()
   email: string;
 
+  @IsNotEmpty()
   @Column()
   position: string;
 
+  @IsNotEmpty()
   @ManyToOne(() => Department, (depart) => depart.users)
   department: Department;
+
+  @IsEmpty()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @IsEmpty()
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
